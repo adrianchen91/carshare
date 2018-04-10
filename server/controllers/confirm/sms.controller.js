d5778f3f6ff9424f755d067f26621573a266c204
Paul Crow <paulcrow@Korriban.local>
2018-04-10 23:40:22 +0800

    WIP add twilio

---
+++ server/controllers/confirm/sms.controller.js
@@ -0,0 +1,44 @@
+const User = require('../../models/user');
+const ConfirmationCode = require('../../models/confirmationCode');
+const sms = require('../../util/sms.helper');
+const codeGenerator = require('../../util/code.generator');
+const dateHelper = require('../../util/date.helper');
+
+const codeTypeEnum = ["Register", "AccountUpdate"];
+
+export const generateConfirmSms = (req, res) => {
+  const codeType = req.body.codeType;
+  const userId = req.userId;
+
+  if (codeTypeEnum.indexOf(codeType) < 0) {
+    res.status(400).send('unknown code type: [' + codeType + ']');
+  } else {
+    User.findById(userId)
+      .then((userErr, user) => {
+        if (userErr) {
+          res.status(404).send('user not found');
+        } else {
+          const code = new ConfirmationCode();
+          code.codeType = codeType;
+          code.code = codeGenerator.generate();
+          code.expiresAt = dateHelper.addHours(new Date(), 0.1667);
+          code.user = user;
+
+          code.save()
+            .then((saveErr) => {
+              if (saveErr) {
+                res.status(500).send('failed to save confirmation code');
+              } else {
+
+                // always return a 200 - we should give them a way to request a new code
+                sms.sendSMS(user.mobile, 'your confirmation code is: ' + code.code)
+                  .then((msg) => res.status(200).send())
+                  .catch(e => res.status(200).send());
+              }
+            });
+        }
+      });
+
+  }
+
+};
